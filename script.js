a1c869027712aaaf62fa5b23b351b8e0d9f8eb10
Renuka Prasad <renu.yarasu@gmail.com>
2022-11-03 13:32:21 +0530

    return

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // Learn With Srikanth Racharla: https://www.youtube.com/watch?v=NHNqHSoU50c&list=PLkusOHO_VvuC5im0DL62_FXPHdfE3GWjd
 console.clear();
- 
+// 1 inch = 2.54 cm
+
+function calculate(value) {
+    return value * 2.54
+}
+;
+
+const width = 0;
+const height = 0;
+const dimensions = [width, height]
+console.log(dimensions);
+console.log(calculate(10));

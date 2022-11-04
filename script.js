07e7e92161721ffde777ebd24848247d5ce0fad0
Renuka Prasad <renu.yarasu@gmail.com>
2022-11-04 14:00:37 +0530

    Conditional Statements

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // Learn With Srikanth Racharla: https://www.youtube.com/watch?v=NHNqHSoU50c&list=PLkusOHO_VvuC5im0DL62_FXPHdfE3GWjd
 console.clear();
-// 1 inch = 2.54 cm
 
-const add = function (num1, num2) {
-    return num1 + num2
+const value = false;
+console.log(typeof value);
+
+if (value) {
+    console.log('Hi');
+} else {
+    console.log('Bye');
 }
-
-const first = add(5, 10);
-const second = add(15, 20);
-
-console.log(first, second, add(100, 200));

ff61a8d038112725d948b11b2c3bb24dd21ecef1
Renuka Prasad <renu.yarasu@gmail.com>
2022-12-13 14:29:26 +0530

    Switch - if

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 // Learn With Srikanth Racharla: https://www.youtube.com/watch?v=NHNqHSoU50c&list=PLkusOHO_VvuC5im0DL62_FXPHdfE3GWjd
 console.clear();
 
-// Logical Operators || OR &&
+// Switch
 
-const name = 'VedaGna';
-const age = 5;
+const dice = 3;
 
-if (name == 'VedaGna' && name == 'Gnapika') {
-    console.log('Hey!');
-} else {
-    console.log('No Comments');
+if (dice === 1) {
+    console.log('One');
 }
-
-// No Comments
+if (dice === 2) {
+    console.log('Two');
+}
+if (dice === 3) {
+    console.log('Three');
+}
+if (dice === 4) {
+    console.log('Four');
+}
+if (dice === 5) {
+    console.log('Five');
+}
+if (dice === 6) {
+    console.log('Six');
+}
+if (dice > 6 || dice < 0) {
+    console.log('No Game');
+} 

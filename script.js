e649ebdca1775f1c4eecceef7123dd637e05169a
Renuka Prasad <renu.yarasu@gmail.com>
2022-12-13 14:52:22 +0530

    Loops - While Loop

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
 // Learn With Srikanth Racharla: https://www.youtube.com/watch?v=NHNqHSoU50c&list=PLkusOHO_VvuC5im0DL62_FXPHdfE3GWjd
 console.clear();
 
-// Switch
+// Loops - While Loop
 
-const dice = 5;
+let amount = 10;
 
-switch (dice) {
-    case 1:
-        console.log('One');
-        break;
-    case 2:
-        console.log('Two');
-        break;
-    case 3:
-        console.log('Three');
-        break;
-    case 4:
-        console.log('Four');
-        break;
-    case 5:
-        console.log('Five');
-        break;
-    case 6:
-        console.log('Six');
-        break;
-
-    default:
-        console.log('No Game');
+while (amount >= 0) {
+    console.log(`I have ${amount} $`);
+    amount--;
 }

1b731da8ef76e9b1cd57a76969a1d17e9dfe613d
Renuka Prasad <renu.yarasu@gmail.com>
2022-12-15 11:50:47 +0530

    Array - Get Full Names

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Learn With Srikanth Racharla: https://www.youtube.com/watch?v=NHNqHSoU50c&list=PLkusOHO_VvuC5im0DL62_FXPHdfE3GWjd
 console.clear();
+/* 
+Connect with JavaScript
+/////////////////////// 
+• String Methods
+• Global/Local Scope
+• Array Iterations (map, filter, reduce, etc)
+• Global Objects (Math, Date)  */
 
-// Loops - While Loop
+let names = ['Veda', 'Gnapika', 'Hindu'];
+let surename = 'Yarasu';
 
-for (let i = 0; i < 10; i++) {
+const myArray = []
+
+for (let i = 0; i < names.length; i++) {
     console.log(i);
-
+    console.log(names[i]);
+    let fullname = `${names[i]} ${surename}`;
+    myArray.push(fullname)
 }
 
+console.log(myArray);

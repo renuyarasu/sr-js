ef4aa92dca571a0311743aaee41dda8a2e14f962
Renuka Prasad <renu.yarasu@gmail.com>
2022-12-21 22:55:32 +0530

    Array Iteration Methods - reduce()

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ console.clear();
 /* 
 https://www.youtube.com/watch?v=84AhO5rHhmA&list=PLkusOHO_VvuC5im0DL62_FXPHdfE3GWjd&index=53
 /////////////////////// 
-// find ()
-// return single instance - (in this case object) 
-// return first match, if no match undefined
-// great for getting unique value  */
+// reduce 
+// iterates, callback function 
+// reduces to a single value - number, array, object 
+// 1 parameter ('acc') - total of all calculations 
+// 2 parameter ('curr') - current iteration/value)   */
 
 const people = [
-    { name: 'Veda', age: 5, class: 'LKG', id: 1 },
-    { name: 'Gnapika', age: 7, class: 'TWO', id: 2 },
-    { name: 'Hindu', age: 25, class: 'Diploma', id: 3 },
-    { name: 'Renu', age: 35, class: 'MBA', id: 4 },
+    { name: 'Veda', age: 5, class: 'LKG', salary: 400 },
+    { name: 'Gnapika', age: 7, class: 'TWO', salary: 300 },
+    { name: 'Hindu', age: 25, class: 'Diploma', salary: 200 },
+    { name: 'Renu', age: 35, class: 'MBA', salary: 100 }
 ]
 
-const findPerson = people.find((person) => person.id === 4);
-console.log(findPerson);
+const newArray = people.reduce((acc, currentPerson) => {
+    console.log(`Total Salary: ${acc}`);
+    console.log(`Current Person Salary: ${currentPerson.salary}`);
+    acc += currentPerson.salary;
+    return acc;
+}, 0)
 
-const findPerson2 = people.filter((person) => person.id === 4);
-console.log(findPerson2[0]);
+console.log(newArray); // 1000

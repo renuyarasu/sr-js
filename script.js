2c718eafa4643238e15840cc7ddaa34aebef6f7f
Renuka Prasad <renu.yarasu@gmail.com>
2022-12-21 23:03:04 +0530

    Math - floor()

--- script.js
+++ script.js
@@ -3,24 +3,9 @@ console.clear();
 /* 
 https://www.youtube.com/watch?v=84AhO5rHhmA&list=PLkusOHO_VvuC5im0DL62_FXPHdfE3GWjd&index=53
 /////////////////////// 
-// reduce 
-// iterates, callback function 
-// reduces to a single value - number, array, object 
-// 1 parameter ('acc') - total of all calculations 
-// 2 parameter ('curr') - current iteration/value)   */
+// Math */
 
-const people = [
-    { name: 'Veda', age: 5, class: 'LKG', salary: 400 },
-    { name: 'Gnapika', age: 7, class: 'TWO', salary: 300 },
-    { name: 'Hindu', age: 25, class: 'Diploma', salary: 200 },
-    { name: 'Renu', age: 35, class: 'MBA', salary: 100 }
-]
+const num = 7.356;
+const result = Math.floor(num)
+console.log(result);
 
-const newArray = people.reduce((acc, currentPerson) => {
-    console.log(`Total Salary: ${acc}`);
-    console.log(`Current Person Salary: ${currentPerson.salary}`);
-    acc += currentPerson.salary;
-    return acc;
-}, 0)
-
-console.log(newArray); // 1000
